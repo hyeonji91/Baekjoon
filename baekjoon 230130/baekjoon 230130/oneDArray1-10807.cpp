ec420d6830cca1576a4b4094abb210c1a6052389
hyeonji91 <hjkim1770@gmail.com>
2023-01-30 20:00:50 +0900

    주석해제

--- baekjoon 230130/baekjoon 230130/oneDArray1-10807.cpp
+++ baekjoon 230130/baekjoon 230130/oneDArray1-10807.cpp
@@ -1,24 +1,24 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include<stdlib.h>
-//
-//int main() {
-//	int* num;
-//	int count;
-//	int searchNum, searchCount=0;
-//	scanf("%d", &count);
-//	num = (int*)malloc(sizeof(int) * count);
-//
-//
-//	for (int i = 0; i < count; i++) {
-//		scanf("%d", &num[i]);
-//	}
-//	scanf("%d", &searchNum);
-//	for (int i = 0; i < count; i++) {
-//		if (searchNum == num[i])
-//			searchCount++;
-//	}
-//	printf("%d", searchCount);
-//	return 0;
-//
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int* num;
+	int count;
+	int searchNum, searchCount=0;
+	scanf("%d", &count);
+	num = (int*)malloc(sizeof(int) * count);
+
+
+	for (int i = 0; i < count; i++) {
+		scanf("%d", &num[i]);
+	}
+	scanf("%d", &searchNum);
+	for (int i = 0; i < count; i++) {
+		if (searchNum == num[i])
+			searchCount++;
+	}
+	printf("%d", searchCount);
+	return 0;
+
+}
--- baekjoon 230130/baekjoon 230130/oneDArray2-10871.cpp
+++ baekjoon 230130/baekjoon 230130/oneDArray2-10871.cpp
@@ -1,17 +1,17 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include<stdlib.h>
-//int main() {
-//	int count, stdNum;
-//	scanf("%d %d", &count, &stdNum);
-//	int* num = (int*)malloc(sizeof(int) * count);
-//	for (int i = 0; i < count; i++) {
-//		scanf("%d", &num[i]);
-//	}
-//	for (int i = 0; i < count; i++) {
-//		if (num[i] < stdNum) {
-//			printf("%d ", num[i]);
-//		}
-//	}
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int count, stdNum;
+	scanf("%d %d", &count, &stdNum);
+	int* num = (int*)malloc(sizeof(int) * count);
+	for (int i = 0; i < count; i++) {
+		scanf("%d", &num[i]);
+	}
+	for (int i = 0; i < count; i++) {
+		if (num[i] < stdNum) {
+			printf("%d ", num[i]);
+		}
+	}
+	return 0;
+}
