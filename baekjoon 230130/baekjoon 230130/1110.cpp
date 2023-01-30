3e00c5a87c5c1579cb00ae32c742c3c2c6375a47
hyeonji91 <hjkim1770@gmail.com>
2023-01-30 19:57:33 +0900

    초기 커밋.

---
+++ baekjoon 230130/baekjoon 230130/1110.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int main() {
+//	int num, sNum = -1;
+//	int ones, tens;
+//	int temp;
+//	int i = 0;
+//	scanf("%d", &sNum);
+//	num = sNum;
+//	tens = num / 10;
+//	ones = num % 10;
+//	temp = ones + tens;
+//	temp = temp % 10;
+//	num = ones * 10 + temp;
+//	i++;
+//	//printf("num=%d  %d\n", num, i);
+//	while (num != sNum) {
+//		tens = num / 10;
+//		ones = num % 10;
+//		temp = ones + tens;
+//		temp = temp % 10;
+//		num = ones * 10 + temp;
+//		i++;
+//		//printf("num=%d  %d\n", num, i);
+//	}
+//	printf("%d", i);
+//	return 0;
+//}
