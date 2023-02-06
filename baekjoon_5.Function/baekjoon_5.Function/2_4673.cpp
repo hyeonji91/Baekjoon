8b97de5a951fb25d217aac78e9d599e2055dc416
hyeonji91 <hjkim1770@gmail.com>
2023-02-06 15:42:19 +0900

    2_4673 더 간단하게 수정

--- baekjoon_5.Function/baekjoon_5.Function/2_4673.cpp
+++ baekjoon_5.Function/baekjoon_5.Function/2_4673.cpp
@@ -3,23 +3,20 @@
 int d(int n, int arr[]) {
 	int temp, th, hun, tens, ones;
 	int r;
-	if (n > MAX) {
+	th = n / 1000;
+	temp = n % 1000;
+	hun = temp / 100;
+	temp = temp % 100;
+	tens = temp / 10;
+	ones = n % 10;
+
+	r = n + th + hun + tens + ones;
+	if (r <= MAX) {
+		arr[r - 1] = r;
+		d(r, arr);
+	}
+	else
 		return 0;
-	}
-	else {
-		th = n / 1000;
-		temp = n%1000;
-		hun = temp / 100;
-		temp = temp%100;
-		tens = temp / 10;
-		ones = n % 10;
-		
-		r = n + th+ hun+ tens + ones;
-		if (r <= MAX) {
-			arr[r - 1] = r;
-			d(r, arr);
-		}
-	}
 }
 int main(void) {
 	int arr[MAX] = { 0 };
