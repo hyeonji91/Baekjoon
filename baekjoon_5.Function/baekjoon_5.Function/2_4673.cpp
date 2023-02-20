21b623106adc73ab5bc68059d82de8679fbfb0ba
hyeonji91 <hjkim1770@gmail.com>
2023-02-20 17:14:35 +0900

    첫번째 for문 조건문 삭제 재귀함수 삭제 -->함수 안 else return0; 삭제 -->함수 자료형 void로 수정

--- baekjoon_5.Function/baekjoon_5.Function/2_4673.cpp
+++ baekjoon_5.Function/baekjoon_5.Function/2_4673.cpp
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #define MAX 10000
 int d(int n, int arr[]) {
-	int temp, th, hun, tens, ones;
-	int r;
+	int temp, th, hun, tens, ones;//순서대로 임시저장, 천의 자리, 백의 자리, 십의 자리, 일의 자리
+	int r;//n을 생성자로 해서 만들어진 수
+
 	th = n / 1000;
 	temp = n % 1000;
 	hun = temp / 100;
@@ -11,22 +12,21 @@ int d(int n, int arr[]) {
 	ones = n % 10;
 
 	r = n + th + hun + tens + ones;
+
 	if (r <= MAX) {
-		arr[r - 1] = r;
-		d(r, arr);
+		arr[r - 1] = 1;
 	}
 	else
 		return 0;
 }
+
 int main(void) {
-	int arr[MAX] = { 0 };
+	int arr[MAX] = { 0, };
 	for (int i = 0; i < MAX; i++) {
-		if (arr[i] == 0) {
-			d(i+1, arr);
-		}
+			d(i+1, arr);//arr의 인덱스는 0부터 시작하지만 주어진 조건은 1부터 10000까지 이므로 i+1
 	}
 	for (int i = 0; i < MAX; i++) {
-		if (arr[i] == 0) {
+		if (arr[i] == 0) {//0이면 해당 수가 셀프넘버임
 			printf("%d\n", i+1);
 		}
 	}
